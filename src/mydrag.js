21b99ba58a8baff3cf26f4f6398849caf053fb48
liuyib <1656081615@qq.com>
2020-03-15 01:19:38 +0800

    style: Perfect codes

--- src/mydrag.js
+++ src/mydrag.js
@@ -3,51 +3,61 @@
 
   function Mydrag(elem, options) {
     this.elem = document.querySelector(elem);
+    // 元素的 getBoundingClientRect 对象
     this.rect = null;
 
+    // 配置参数
     this.config = this.mergeConfig(options);
 
+    // 窗口尺寸
     this.winW = window.innerWidth;
     this.winH = window.innerHeight;
 
-    this.x = 0;
-    this.y = 0;
-
+    // 元素最初的坐标
     this.oldX = 0;
     this.oldY = 0;
 
-    // 元素的限制范围
+    // 元素当前坐标
+    this.x = 0;
+    this.y = 0;
+
+    // 元素坐标的限制范围
     this.limit = {
       l: 0, // left
       r: 0, // right
       t: 0, // top
       b: 0, // bottom
     };
-    this.gap = this.config.gap; // 安全边距
-    this.areaId = 0;  // 区域 ID（0：左上，1：右上，2：左下，3：右下）
+    // 安全边距
+    this.gap = this.config.gap;
+    // 区域 ID（0：左上，1：右上，2：左下，3：右下）
+    this.areaId = 0;
 
     this.init();
   }
 
   window['Mydrag'] = Mydrag;
 
+  // 用户可配置的 API
   Mydrag.config = {
-    adsorb: true, // 是否自动吸附边缘
-    initX: 0, // 初始 x 坐标（单位 px）
-    initY: 0, // 初始 y 坐标（单位 px）
-    gap: 10,  // 安全边距（单位 px）
+    adsorb: true,  // 是否自动吸附边缘
+    initX: 0,      // 初始 x 坐标（单位 px）
+    initY: 0,      // 初始 y 坐标（单位 px）
+    gap: 10,       // 安全边距   （单位 px）
   };
 
+  // 事件名称
   Mydrag.events = {
     TOUCH_START: 'touchstart',
     TOUCH_MOVE: 'touchmove',
-    TOUCH_END: 'touchend'
+    TOUCH_END: 'touchend',
   };
 
   Mydrag.prototype = {
     init() {
       this.rect = this.elem.getBoundingClientRect();
 
+      // 初始元素的坐标范围
       this.limit.l = this.gap;
       this.limit.r = this.winW - this.rect.width - this.gap;
       this.limit.t = this.gap;
@@ -56,7 +66,7 @@
       // 初始元素坐标
       this.setPos(this.config.initX, this.config.initY);
 
-      // 开始监听鼠标事件
+      // 监听鼠标事件
       this.startListening();
     },
     /**
@@ -64,7 +74,6 @@
      */
     startListening() {
       var isPassive = this.detectPassive();
-
       this.elem.addEventListener('touchstart', this);
       this.elem.addEventListener('touchmove', this, isPassive);
       this.elem.addEventListener('touchend', this);
@@ -99,6 +108,10 @@
         }
       }.bind(this))(e.type, Mydrag.events);
     },
+    /**
+     * 触摸元素时调用
+     * @param {Object} event 事件对象
+     */
     moveStart(event) {
       var ev = (event.touches && event.touches[0]) || event;
 
@@ -106,6 +119,10 @@
       this.oldX = this.oldX || ev.clientX;
       this.oldY = this.oldY || ev.clientY;
     },
+    /**
+     * 移动元素时调用
+     * @param {Object} event 事件对象
+     */
     moving(event) {
       var ev = (event.touches && event.touches[0]) || event;
 
@@ -126,16 +143,21 @@
       // 区域检测
       this.detectArea();
     },
+    /**
+     * 释放元素时调用
+     */
     moveEnd() {
+      // 移除事件监听器
       this.stopListener();
 
-      // 根据元素释放时所在的区域来吸附边缘
+      // 根据元素释放时所在的区域，判断要吸附的边缘
       var targetX = this.x;
       if (this.areaId === 0 || this.areaId === 2) {
         targetX = this.limit.l;
       } else if (this.areaId === 1 || this.areaId === 3) {
         targetX = this.limit.r;
       }
+      // 执行吸附动画
       this.easeout(this.x, targetX, 4, function (val) {
         this.x = val;
         this.setPos(this.x, this.y);
@@ -186,6 +208,7 @@
         this.y = this.limit.b;
       }
 
+      // 元素位置超出范围时进行限制
       if (isOverflow) {
         this.setPos(this.x, this.y);
       }
